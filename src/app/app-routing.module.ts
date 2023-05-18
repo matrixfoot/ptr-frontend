919c374c08d6047e98e92229a83d59c01fdea2ca
kammounomarindus@gmail.com <kammounomarindus@gmail.com>
2023-05-18 16:43:59 +0100

    tgetyg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { ContactComponent } from './home/contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { WhoComponent } from './who/who.component';
-import { WhyComponent } from './why/why.component';
 import { ProfilComponent } from './profil/profil.component';
 import { AdminBoardComponent } from './admin-board/admin-board.component';
 import { UserBoardComponent } from './user-board/user-board.component';
@@ -18,28 +16,14 @@ import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ComingSoonPageComponent } from './coming-soon-page/coming-soon-page.component';
-import { CareerComponent } from './career/career.component';
-import { ViewCondidateComponent } from './view-condidate/view-condidate.component';
-import { ModifyCondidateComponent } from './modify-condidate/modify-condidate.component';
-import { DeclareFiscalityComponent } from './declare-fiscality/declare-fiscality.component';
-import { ViewContactreqComponent } from './view-contactreq/view-contactreq.component';
+
 import { ModifyContactreqComponent } from './modify-contactreq/modify-contactreq.component';
 import { CompleteProfilComponent } from './complete-profil/complete-profil.component';
-import { CalendarFiscalityComponent } from './calendar-fiscality/calendar-fiscality.component';
 import { CollabUserCreateComponent } from './collab-user-create/collab-user-create.component';
-import { AddEventComponent } from './calendar-fiscality/add-event/add-event.component';
-import { ViewEventComponent } from './view-event/view-event.component';
-import { ModifyEventComponent } from './modify-event/modify-event.component';
+
 import { SettingsComponent } from './settings/settings.component';
-import { DeclareComptabiliteComponent } from './declare-comptabilite/declare-comptabilite.component';
 import { ViewUserDeletedComponent } from './view-user-deleted/view-user-deleted.component';
-import { ViewDecFiscMensComponent } from './view-dec-fisc-mens/view-dec-fisc-mens.component';
-import { ModifyDecFiscMensComponent } from './modify-dec-fisc-mens/modify-dec-fisc-mens.component';
-import { CreateReportComponent } from './create-report/create-report.component';
-import { ViewCarouselComponent } from './view-carousel/view-carousel.component';
-import { ModifyCarouselComponent } from './modify-carousel/modify-carousel.component';
-import { ModifyDeccomptabiliteComponent } from './modify-deccomptabilite/modify-deccomptabilite.component';
-import { ViewDeccomptabiliteComponent } from './view-deccomptabilite/view-deccomptabilite.component';
+
 import { CollabBoardComponent } from './collab-board/collab-board.component';
 
 const routes: Routes = [
@@ -55,80 +39,30 @@ const routes: Routes = [
 { path: 'settings', component: SettingsComponent,
     
 },
-{ path: 'create-report', component: CreateReportComponent,
-    
-},
-{ path: 'declare-fiscality', component:DeclareFiscalityComponent,canDeactivate: [CanDeactivateGuard]
-    
-},
-{ path: 'declare-comptabilite', component:DeclareComptabiliteComponent,canDeactivate: [CanDeactivateGuard]
-    
-},
-{ path: 'calendar-fiscality', component:CalendarFiscalityComponent,
-    
-},
-{ path: 'settings/calendar-fiscality', component:CalendarFiscalityComponent,
-    
-},
-{ path: 'home/calendar-fiscality', component:CalendarFiscalityComponent,
-    
-},
-{ path: 'login/calendar-fiscality', component:CalendarFiscalityComponent,
-    
-},
-{ path: 'add-event', component:AddEventComponent,
-    
-},
 { path: 'modify-user/:id', component: ModifyUserComponent,
     
 },
 { path: 'complete-profil/:id', component: CompleteProfilComponent,
     
 },
-{ path: 'modify-condidate/:id', component: ModifyCondidateComponent,
     
-},
+
 { path: 'modify-contactreq/:id', component: ModifyContactreqComponent,
     
 },
 { path: 'modify-user-admin/:id', component: ModifyUserAdminComponent,
     
 },
-{ path: 'modify-event/:id', component: ModifyEventComponent,
-    
-},
-{ path: 'modify-decfiscmens/:id', component: ModifyDecFiscMensComponent,canDeactivate: [CanDeactivateGuard]
-    
-},
-{ path: 'admin-board/modify-decfiscmens/:id', component: ModifyDecFiscMensComponent
-    
-},
-{ path: 'supervisor-board/modify-decfiscmens/:id', component: ModifyDecFiscMensComponent
-    
-},
-{ path: 'modify-deccomptabilite/:id', component: ModifyDeccomptabiliteComponent,canDeactivate: [CanDeactivateGuard]
-    
-},
-{ path: 'modify-carousel/:id', component: ModifyCarouselComponent
-    
-},
+
 { path: 'view-user/:id', component: ViewUserComponent},
 { path: 'view-user-deleted/:id', component: ViewUserDeletedComponent},
-{ path: 'view-condidate/:id', component: ViewCondidateComponent},
-{ path: 'view-contactreq/:id', component: ViewContactreqComponent},
-{ path: 'view-decfiscmens/:id', component: ViewDecFiscMensComponent},
-{ path: 'view-deccomptabilite/:id', component: ViewDeccomptabiliteComponent},
 
-{ path: 'view-carousel/:id', component: ViewCarouselComponent},
-
-{ path: 'view-event/:id', component: ViewEventComponent},
 { path: 'verify-email/:token', component: VerifyEmailComponent},
 { path: 'reset-password/:token', component: ResetPasswordComponent},
 { path: 'admin-board', component: AdminBoardComponent},
 { path: 'user-board', component: UserBoardComponent},
 { path: 'collab-board', component: CollabBoardComponent},
 
-{ path: 'career', component: CareerComponent},
 { path: 'supervisor-board', component: SupervisorBoardComponent},
 
   {path: 'home/contact', component :ContactComponent},
@@ -150,12 +84,7 @@ const routes: Routes = [
 { path: 'admin-board/profil', component: ProfilComponent,
     
 },
-{ path: 'why', component: WhyComponent,
-    
-},
-{ path: 'who', component: WhoComponent,
-    
-},
+
 { path: 'coming-soon', component: ComingSoonPageComponent,
     
 },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,8 +23,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth-interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { WhoComponent } from './who/who.component';
-import { WhyComponent } from './why/why.component';
 import { ContactComponent } from './home/contact/contact.component';
 import { ProfilComponent } from './profil/profil.component';
 import { AdminBoardComponent } from './admin-board/admin-board.component';
@@ -37,42 +35,29 @@ import { ModifyUserAdminComponent } from './modify-user-admin/modify-user-admin.
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { CareerComponent } from './career/career.component';
 import { ComingSoonPageComponent } from './coming-soon-page/coming-soon-page.component';
-import { ViewCondidateComponent } from './view-condidate/view-condidate.component';
-import { ModifyCondidateComponent } from './modify-condidate/modify-condidate.component';
-import { DeclareFiscalityComponent } from './declare-fiscality/declare-fiscality.component';
+
 import { AlertComponent } from './alert/alert.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { DueDateTimelineComponent } from './due-date-timeline/due-date-timeline.component';
-import { ViewContactreqComponent } from './view-contactreq/view-contactreq.component';
+
 import { ModifyContactreqComponent } from './modify-contactreq/modify-contactreq.component';
 import { CompleteProfilComponent } from './complete-profil/complete-profil.component';
-import { CalendarFiscalityComponent } from './calendar-fiscality/calendar-fiscality.component';
 import { CollabUserCreateComponent } from './collab-user-create/collab-user-create.component';
 import { SortDirective } from './directive/sort.directive';
 import { TwoDigitDecimaNumberDirective } from './directive/TwoDigitDecimaNumber.directive';
 import { RemoveCommaPipe } from './pipes/pipe-number';
 
-import { AddEventComponent } from './calendar-fiscality/add-event/add-event.component';
 import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
 import interactionPlugin from '@fullcalendar/interaction';
-import { ViewEventComponent } from './view-event/view-event.component';
-import { ModifyEventComponent } from './modify-event/modify-event.component';
+
 import { SettingsComponent } from './settings/settings.component';
 import { HttpErrorInterceptor } from './interceptors/error-interceptor';
 import { ExcelService } from './services/excel.service';
-import { DeclareComptabiliteComponent } from './declare-comptabilite/declare-comptabilite.component';
 import { ViewUserDeletedComponent } from './view-user-deleted/view-user-deleted.component';
-import { ViewDecFiscMensComponent } from './view-dec-fisc-mens/view-dec-fisc-mens.component';
-import { ModifyDecFiscMensComponent } from './modify-dec-fisc-mens/modify-dec-fisc-mens.component';
+
 import { CanDeactivateGuard } from './services/auth-guard.service';
-import { CreateReportComponent } from './create-report/create-report.component';
-import { ViewCarouselComponent } from './view-carousel/view-carousel.component';
-import { ModifyCarouselComponent } from './modify-carousel/modify-carousel.component';
-import { ModifyDeccomptabiliteComponent } from './modify-deccomptabilite/modify-deccomptabilite.component';
-import { ViewDeccomptabiliteComponent } from './view-deccomptabilite/view-deccomptabilite.component';
+
 import { CollabBoardComponent } from './collab-board/collab-board.component';
 
 FullCalendarModule.registerPlugins([
@@ -93,8 +78,6 @@ FullCalendarModule.registerPlugins([
    
     FooterComponent,
     HomeComponent,
-    WhoComponent,
-    WhyComponent,
     ContactComponent,
     ProfilComponent,
     AdminBoardComponent,
@@ -108,34 +91,20 @@ FullCalendarModule.registerPlugins([
     VerifyEmailComponent,
     ForgotPasswordComponent,
     ResetPasswordComponent,
-    CareerComponent,
     ComingSoonPageComponent,
-    ViewCondidateComponent,
-    ModifyCondidateComponent,
-    DeclareFiscalityComponent,
+    
     AlertComponent,
     SearchBarComponent,
-    DueDateTimelineComponent,
-    ViewContactreqComponent,
+  
     ModifyContactreqComponent,
     CompleteProfilComponent,
-    CalendarFiscalityComponent,
     CollabUserCreateComponent,
     SortDirective,
     TwoDigitDecimaNumberDirective,
-    AddEventComponent,
-    ViewEventComponent,
-    ModifyEventComponent,
+   
     SettingsComponent,
-    DeclareComptabiliteComponent,
     ViewUserDeletedComponent,
-    ViewDecFiscMensComponent,
-    ModifyDecFiscMensComponent,
-    CreateReportComponent,
-    ViewCarouselComponent,
-    ModifyCarouselComponent,
-    ModifyDeccomptabiliteComponent,
-    ViewDeccomptabiliteComponent,
+    
     CollabBoardComponent,
     
     
